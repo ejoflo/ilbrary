c71ce97c5f3fdf71ed4321c85ba30f55abd92431
Ejoflo <ejoflo@icloud.com>
2019-09-03 20:11:00 -1000

    initial commit

---
+++ book.js
@@ -0,0 +1,16 @@
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = true;
+    
+}
+    
+// this.info = function() {
+    // return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+
+// title
+// author
+// pages
+// read
+// info
