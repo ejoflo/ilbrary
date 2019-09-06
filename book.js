ff0505a43f1d52699fa17f0df51063116c78df10
Ejoflo <ejoflo@icloud.com>
2019-09-05 15:53:24 -1000

    add all features

--- book.js
+++ book.js
@@ -1,46 +1,73 @@
 const library = document.querySelector('#libraryContainer');
 const bookInput = document.querySelector('#bookInput');
-const bookTable = document.querySelector('#bookTable');
+const submitButton = document.querySelector('#submit');
 
 let myLibrary = [];
+
+// Book.prototype.info = function() {   // info
+//     return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+// }
 
 function Book(title, author, pages, read) {   // book constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;   
+    this.read = read;
+}
+
+function removeBook(bookIndex) {
+    let bookNum = bookIndex.slice(9, bookIndex.length);
+    let bookCard = document.querySelector('.'+ bookIndex)
+    
+    myLibrary.splice(bookNum, 1);
+    myLibrary.forEach(function(element, index, theArray) {   // update the Array index
+        myLibrary[index]['index'] = index;
+    });
+    clearLibrary();
+    render(myLibrary);
+    createListeners();
+}
+
+function addBookToLibrary(obj) {
+    myLibrary.push(obj);
+    obj.index = myLibrary.length-1;
 }
 
 function clearLibrary() {
     while (library.firstChild) {
         library.removeChild(library.firstChild);
     }
+    clearForm();
+    bookInput.style.display = 'none';
 }
 
-function removeBook(bookIndex) {
-    let bookCard = document.querySelector('.'+ bookIndex)
-    library.removeChild(bookCard);
-}
-
-function addBookToLibrary(obj) {
-    myLibrary.unshift(obj);
-}
-
-function newBoko() {
+function clearForm() {
     const form = document.querySelector('form');
     const titleInput = form.titleInput;
     const authorInput = form.authorInput;
     const pagesInput = form.pagesInput;
     const readYesInput = form.readYesInput;
-    const readNoInput = form.readYesInput;
+    
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readYesInput.checked = false;
+}
+
+function newBook() {
+    const form = document.querySelector('form');
+    const titleInput = form.titleInput;
+    const authorInput = form.authorInput;
+    const pagesInput = form.pagesInput;
+    const readYesInput = form.readYesInput;
     
     let newTitle = titleInput.value;
     let newAuthor = authorInput.value;
     let newPages = +pagesInput.value;
-    let newRead;
+    let newRead = readYesInput.checked;
     let newBook;
 
-    if (readYesInput.value === 'yes') {
+    if (newRead === true) {
         newRead = 'Yes';
     } else {
         newRead = 'No';
@@ -51,35 +78,30 @@ function newBoko() {
     addBookToLibrary(newBook);
     clearLibrary();
     render(myLibrary);
-    bookInput.style.display = 'none';
-
+    createListeners();
 }
-
-// Book.prototype.info = function() {   // info
-//     return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
-// }
-
-let bookOne = new Book ('Random Book', 'Randall Randy', 120, 'Yes');
-let bookTwo = new Book ('Whatever Dis Crap Called', 'Wanda Whatever', 200, 'No');
-let bookThree = new Book('Stupid Shitboi', 'Moby Dickwad', 1000, 'Yes');
 
 function render(arr) {
     arr.forEach(function(element, index, theArray) {
         library.appendChild(document.createElement('div')).classList.add('bookIndex' + '' + index);
-        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).innerHTML = 'Title: ' + theArray[index]['title'];
-        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).innerHTML = 'Author: ' + theArray[index]['author'];
-        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).innerHTML = 'Pages: ' + theArray[index]['pages'];
-        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).innerHTML = 'Read? ' + theArray[index]['read'];
+        // document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).innerHTML = 'Title: ' + theArray[index]['title'];
+        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).insertAdjacentHTML('beforeend', 'Title: ' + theArray[index]['title']);
+        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).insertAdjacentHTML('beforeend', 'Author: ' + theArray[index]['author']);
+        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).insertAdjacentHTML('beforeend', 'Pages: ' + theArray[index]['pages']);
+        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('p')).insertAdjacentHTML('beforeend', 'Read? ' + theArray[index]['read']);
+        document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('button')).setAttribute('id', 'readStatus' + '' + index);
+        document.querySelector('#readStatus' + '' + index).insertAdjacentHTML('beforeend', 'Read Status');
+        document.querySelector('#readStatus' + '' + index).setAttribute('class', 'readStatusBtn');
         document.querySelector('.bookIndex' + '' + index).appendChild(document.createElement('button')).setAttribute('id', 'bookIndex' + '' + index);
         document.querySelector('#bookIndex' + '' + index).setAttribute('class', 'removeBtn');
-        document.querySelector('#bookIndex' + '' + index).innerHTML = 'Remove';
+        document.querySelector('#bookIndex' + '' + index).insertAdjacentHTML('beforeend', 'Remove');
     });
 }
 
 function createListeners() {
     const newButton = document.querySelector('#newBook');
     const removeButtons = document.querySelectorAll('.removeBtn');
-    const submitButton = document.querySelector('#submit');
+    const readStatusButtons = document.querySelectorAll('.readStatusBtn');
 
     newButton.addEventListener('click', (e) => {
         bookInput.style.display = 'block';
@@ -91,11 +113,38 @@ function createListeners() {
         });
     });
 
-    submitButton.addEventListener('click', (e) => {
-        // document.forms["bookForm"].submit();
-        newBoko();
+    readStatusButtons.forEach((button) => {
+        let bookNum = button.id;
+        
+        bookNum = bookNum.slice(10, bookNum.length);
+
+        button.addEventListener('click', (e) => {
+            console.log(document.getElementById(button.id).previousElementSibling);
+            
+            if (document.getElementById(button.id).previousElementSibling.innerHTML === 'Read? Yes') {
+                document.getElementById(button.id).previousElementSibling.innerHTML = 'Read? No';
+                myLibrary[bookNum]['read'] = 'No';
+                clearLibrary();
+                render(myLibrary);
+                createListeners();
+            } else {
+                document.getElementById(button.id).previousElementSibling.innerHTML = 'Read? Yes';
+                myLibrary[bookNum]['read'] = 'Yes';
+                clearLibrary();
+                render(myLibrary);
+                createListeners();
+            }
+        });
     });
 }
+
+let bookOne = new Book ('Random Book', 'Randall Randy', 120, 'Yes');
+let bookTwo = new Book ('Whatever Dis Crap Called', 'Wanda Whatever', 200, 'No');
+let bookThree = new Book('Stupid Boi', 'Moby Dick', 1000, 'Yes');
+
+submitButton.addEventListener('click', (e) => {
+    newBook();
+});
 
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
